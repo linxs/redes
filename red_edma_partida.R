8e9080e9faacb9d95699b4595e4800744636ba03
linxs <federicolotto@gmail.com>
2018-04-05 15:53:47 -0300

    Red EDMA partida

---
+++ red_edma_partida.R
@@ -0,0 +1,120 @@
+library(plyr)
+#v<-c(0,2,0,1,0,0,0,-1,0,-2,   -1.5,1, -1,1, -1.5,0, -1,0, -1.5,-1, -1,-1, 1,1, 1.5,1,1,0, 1.5,0, 1,-1, 1.5,-1.0   )
+w<-c(0,2.1,0,1,0,0,0,-1,0,-2,   -1.5,1, -1,1, -1.6,0, -1,0, -1.5,-1, -1,-1, 1,1.1, 1.5,1,1,0, 1.5,0, 1,-1, 1.5,-1.0   )
+#x<-c(0,2,0,1,0.15,0,0,-1,0,-2,   -1.5,.97, -1,1, -1.5,0, -1,0, -1.5,-1, -1,-1, .86,1, 1.5,1,1,0, 1.5,0, 1,-1, 1.5,-1.0   )
+#y<-c(0,2,0,1,0,0,0.08,-1,0,-2,   -1.5,1, -1,1, -1.39,0, -1,0, -1.5,-1, -1,-1, 1,1, 1.54,1,1,0, 1.5,0, 1,-1, 1.5,-1.0   )
+#z<-c(0,2,0,1,0,0.13,0,-1,0,-2,   -1.5,1, -1,1, -1.57,0, -1,0, -1.5,-1, -1.1,-1, 1,1, 1.5,1,1,0, 1.5,0, 1.3,-1, 1.5,-1.0   )
+#vp<-c(v,w,x,y,z)
+
+
+k<-2#dimensiones
+sn<-10000#numero de especimenes
+ln<-17#numero de landmarks
+n<-4
+des<-0.05
+
+B<-array(w,c(k,ln,sn))
+
+O<-B
+
+for(j in 1:sn){
+  lp<-sample(c(1:17),n,replace = FALSE)
+  B[,lp,j]<-(rnorm(length(lp)*k,mean = B[,lp,j],sd=des))
+}
+
+E<-B-O#saca el error por coord de cada landmark
+
+
+#Aca guardar los originales
+
+#
+
+#rotacion
+alfa<-runif(sn,min = 0,max = pi/20)
+AR<-sapply(alfa,simplify = "array",function(x){matrix(c(cos(x),-sin(x),sin(x),cos(x)),2,2)})
+TAR<-array(0,c(k,ln,sn))
+for(s in 1:sn){
+  TAR[,,s]<-AR[,,s]%*%B[,,s]
+}
+
+#traslacion
+TAR<-aaply(TAR,3,function(x){
+  x+matrix(runif(2),k,ln)})
+TTAR<-aperm(TAR,c(3,2,1))
+
+#----------seccion NN --------------------
+
+base<-matrix(0,sn,((ln*ln)-ln)/2,byrow=T)#arma una matriz con las coordenadas
+for(i in 1:sn){
+  base[i,]<-c(dist(TTAR[,,i]))
+}
+
+
+
+
+EP<-aperm(E,c(2,1,3))
+orig<-matrix(EP,sn,ln*2,byrow=T)#errores por coord
+
+
+mat<-cbind(base,orig)#esta matriz de dimensiones c(10000,136+34) tiene la base completa (train y test), con x (,1:136) e y(,137:170)
+
+#----------------
+
+
+# Split
+train = mat[1:9000,]
+test = mat[9001:10000,]
+
+
+# Cre Max y Min
+maxs <- apply(train[,1:136], 2, max)
+mins <- apply(train[,1:136], 2, min)
+
+# escalado y vuelto a la matriz mat
+train[,1:136] <- scale(train[,1:136],center = TRUE, scale = maxs - mins)
+test[,1:136] <- scale(test[,1:136],center = TRUE, scale = maxs - mins)
+
+x_train <- train[,1:136]
+y_train <- train[,137:170]
+x_test <- test[,1:136]
+y_test <- test[,137:170]
+
+
+#---------KERAS-----------
+
+
+library(keras)
+
+#define el modelo
+model <- keras_model_sequential()
+model %>% 
+  layer_dense(units = 272, activation = 'relu', input_shape = c(136)) %>%
+  layer_dense(units = 272, activation = 'relu') %>%
+  layer_dense(units = 272, activation = 'relu') %>%
+  layer_dense(units = 34, activation = 'linear')
+
+
+
+#compila el modelo
+model %>% compile(
+  loss = "mean_squared_error",
+  optimizer=optimizer_adam(lr = 0.0001),
+  metrics = "mean_squared_error"
+)
+
+
+#entrena el modelo
+inicio<-Sys.time()
+history <- model %>% fit( x_train, y_train,
+                          epochs = 50,
+                          batch_size = 10,
+                          validation_split = 0.1)
+
+
+pp<-predict(model,x_test)
+
+for(i in 1:30){
+  plot(t(O[,,i])+matrix(pp[i,],ncol = 2),col="red")
+  points(t(B[,,9000+i]))
+  readline(prompt="Press [enter] to continue")
+}
